beb4ab56a32b51c781b047cb9eae12ea12db4515
Tamir <tamir@htld>
2025-01-26 19:26:15 +0100

    Link to / added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,15 @@ const Header = () => {
             <p className="mr-20 text-white text-8xl text-right font-Jaro">HTL Dornbirn 5bWI</p>
             <Link
                 to="/homework"
-                className="cursor-pointer ml-14 mb-14 p-5 text-white border-white border-2 p-3"
+                className="hover:bg-white hover:text-black cursor-pointer ml-14 mb-14 p-5 text-white border-white border-2 p-3"
             >
                 Homework page
+            </Link>
+            <Link
+                to="/"
+                className="hover:bg-white hover:text-black cursor-pointer ml-14 mb-14 p-5 text-white border-white border-2 p-3"
+            >
+                Cards
             </Link>
         </div>
     );
